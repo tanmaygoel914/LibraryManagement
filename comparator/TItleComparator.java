a0dfba0a451727321e5bdfe12241cf4b59cd5f00
tanmaygoel914 <goel.tanmay914@gmail.com>
2025-04-25 16:32:15 +0530

    TitleCom

---
+++ comparator/TItleComparator.java
@@ -0,0 +1,9 @@
+package comparator;
+import entity.Book;
+import java.util.Comparator;
+public class TItleComparator implements Comparator<Book>{
+    @Override
+    public int compare(Book o1, Book o2) {
+        return o1.getTitle().compareTo(o2.getTitle());
+    }
+}
